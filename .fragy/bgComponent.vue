be9c133015e02a39521156d3c95fbd8c222447c0
Rorical <46294886+Rorical@users.noreply.github.com>
2022-09-05 20:42:40 +0800

    update

---
+++ .fragy/bgComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  Hello, World!
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "bgComponent"
+  })
+</script>
--- fragy.config.js
+++ fragy.config.js
@@ -3,13 +3,65 @@ export default {
   subtitle: 'Talk is cheap, show me your code. Code is cheap, show me your proof.',
   locale: 'zh-CN',
   theme: {
-    package: "@fragy/purity",
+    package: "@fragy/oneslot",
     config: {
-      valine: {
+      colors: {
+        default: {
+          primary: '#636462',
+          text: '#636462',
+          textSecondary: '#d8d6d9',
+          textHighlight: '#464843',
+          link: '#828483',
+          navText: '#929493',
+          pageBackground: '#fafbfc',
+          bannerBackground: '#d1d1d1',
+          articleBackground: '#fdfdfd',
+          codeBackground: 'rgba(36, 38, 39, 0.9)',
+          codeBarBackground: 'rgba(62, 63, 66, 0.6)',
+          tagBackground: 'rgba(92, 95, 93, 0.875)',
+          shadow: 'rgba(16, 17, 15, 0.06)',
+          lightShadow: 'rgba(16, 17, 15, 0.02)',
+          textShadow: 'rgba(8, 12, 10, 0.08)',
+          border: 'rgba(0, 0, 0, 0.06)',
+          blockquoteBg: '#f7f6f9',
+          blockquoteBorder: '#f5f5f6',
+          blockquoteSecondaryBg: '#fbfcfc',
+          blockquoteSecondaryBorder: '#f3f4f4',
+        },
+        dark: {
+          pageBackground: '#232425',
+        },
+      },
+      backToTop: true,
+      filterType: 'category', // category or tags
+      fontFamily: 'Noto Sans SC', // Main font family for your site
+      footer: {
+        copyright: {
+          show: true,
+          name: '{domain}',
+        },
+        poweredBy: true,
+      },
+      article: {
+        title: '{articleTitle} - {siteTitle}',
+      },
+      landing: {
+        banner:{
+          bgCompoenent: "bgComponent"
+        }
+      },
+      gfont: {
         enable: true,
-        appId: 'HNbv8EjPb4szlFyIEHAKNLf5-gzGzoHsz',
-        appKey: 'siIEAqLhUlT3P1pU53hpHED8',
-        placeholder: '可以来评论啦！'
+        family: 'Noto+Sans+SC:wght@100;300;400;500;700;900',
+        googleApisHost: '', // default: fonts.googleapis.com
+        gstaticHost: '', // default: fonts.gstatic.com
+      },
+      vendors: {
+        highlightjs: {
+          main: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js',
+          theme:
+            'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github-dark.min.css',
+        },
       },
     }
   }
